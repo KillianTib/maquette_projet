8822853203b28ca9b3fd3f4b829672c78a4d9962
Bastiennbr <164022475+Bastiennbr@users.noreply.github.com>
2025-01-23 21:43:30 +0100

    Changement hauteur de la section-image-carrousel

--- console.js
+++ console.js
@@ -6,7 +6,7 @@ let index = 4;
 function updateCarrousel() {
   const containerWidth = document.querySelector('.conteneur-carrousel').offsetWidth;
   const centerOffset = (containerWidth - 210) / 2;
-  const translation = -(index * 290) + centerOffset;
+  const translation = -(index * 280) + centerOffset;
 
 
   carrousel.style.transition = 'transform 0.5s ease-in-out';
