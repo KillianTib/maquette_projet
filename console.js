ca2694a9906d25daf91b3787a61c0c69118543a4
Bastiennbr <164022475+Bastiennbr@users.noreply.github.com>
2025-01-26 20:53:22 +0100

    Annulation des changement pour le pull request
    Problème de conflits avec Grande Images

---
+++ console.js
@@ -0,0 +1,63 @@
+const carrousel = document.querySelector('.carrousel');
+const images = document.querySelectorAll('.image-carrousel');
+
+let index = 4;
+
+function updateCarrousel() {
+  const containerWidth = document.querySelector('.conteneur-carrousel').offsetWidth;
+  const centerOffset = (containerWidth - 210) / 2;
+  const translation = -(index * 280) + centerOffset;
+
+
+  carrousel.style.transition = 'transform 0.5s ease-in-out';
+  carrousel.style.transform = `translateX(${translation}px)`;
+}
+
+images.forEach((image, i) => {
+  image.addEventListener('click', () => {
+    index = i;
+    updateCarrousel();
+  });
+});
+
+window.addEventListener('resize', () => {
+  carrousel.style.transition = 'none';
+  updateCarrousel();
+});
+
+updateCarrousel();
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const boutonRecherche = document.getElementById('icon-loupe');
+  const barreRecherche = document.querySelector('.barre-recherche');
+
+  boutonRecherche.addEventListener('click', () => {
+      barreRecherche.classList.toggle('actif');
+  });
+});
+
+function changerImage(image) {
+  document.getElementById('imagePrincipale').src = image;
+}
+
+window.onload = () => {
+  const boutons = document.querySelectorAll('.cercle-bouton');
+  if (boutons.length > 0) {
+    boutons[0].classList.add('active'); 
+  }
+};
+
+function changerImage(image) {
+  document.getElementById('imagePrincipale').src = image;
+
+  const boutons = document.querySelectorAll('.cercle-bouton');
+  boutons.forEach((bouton) => bouton.classList.remove('active'));
+  const boutonActif = Array.from(boutons).find(
+    (bouton) => bouton.getAttribute('onclick').includes(image)
+  );
+  if (boutonActif) {
+    boutonActif.classList.add('active');
+  }
+}
